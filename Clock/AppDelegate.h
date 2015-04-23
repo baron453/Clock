c1165d38978cbca9aff3fd0da5c03b9c177dd961
tran nam <trannam@trans-MacBook-Air.local>
2015-04-23 10:15:36 +0700

    Initial Commit

---
+++ Clock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Clock
+//
+//  Created by tran nam on 4/23/15.
+//  Copyright (c) 2015 tran nam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Clock/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Clock
+//
+//  Created by tran nam on 4/23/15.
+//  Copyright (c) 2015 tran nam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
